1f37885d51837a5e30ff82cc583ca32ce9a2e901
dave17code <dave17code@gmail.com>
2024-12-17 13:31:56 +0900

    feat: 메인 페이지 레이아웃 틀잡기  작업

---
+++ src/app/main/page.tsx
@@ -0,0 +1,22 @@
+export default function Main() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {/* Content 1 */}
+      <div className="w-[1280px] h-[720px] bg-blue-500 flex items-center justify-center text-white text-2xl font-bold m-4">
+        Content 1
+      </div>
+      {/* Content 2 */}
+      <div className="w-[1280px] h-[720px] bg-green-500 flex items-center justify-center text-white text-2xl font-bold m-4">
+        Content 2
+      </div>
+      {/* Content 3 */}
+      <div className="w-[1280px] h-[720px] bg-red-500 flex items-center justify-center text-white text-2xl font-bold m-4">
+        Content 3
+      </div>
+      {/* Content 4 */}
+      <div className="w-[1280px] h-[720px] bg-yellow-500 flex items-center justify-center text-white text-2xl font-bold m-4">
+        Content 4
+      </div>
+    </div>
+  );
+}
